222b2c2055a9ab148c51ccef4ca2d22d68c022bd
Jacob Erasmus <j.g.erasmus@outlook.com>
2026-08-14 12:04:05 +0200

    confusion matrix generation code

--- 5_sequential_hybrid_ensemble.py
+++ 5_sequential_hybrid_ensemble.py
@@ -247,6 +247,29 @@ joblib.dump(layer1_ocsvm, os.path.join(export_dir, 'seq_layer1_ocsvm.pkl'))
 joblib.dump(layer2_xgb, os.path.join(export_dir, 'seq_layer2_xgb.pkl'))
 print("     [>] Scaler, Layer 1, and Layer 2 succuessfully saved to disk.")
 
+################################
+# 9. Confusion Matrix generation
+################################
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import confusion_matrix
+
+v_directory = "visualisations"
+os.makedirs(v_directory, exist_ok=True)
+
+print(f"\f[+] Generating Confusion Matrix")
+cfm = confusion_matrix(y_test, final_predictions)
+plt.figure(figsize=(10, 8))
+sns.heatmap(cfm, annot=True, fmt='d', cmap='Blues', cbar=False, linewidths=0.5, linecolor='black')
+plt.title('Hybrid Ensemble - Confusion Matrix', fontsize=14, pad=15)
+plt.ylabel('True Network State', fontsize=12)
+plt.xlabel('Predicted Network State', fontsize=12)
+plt.tight_layout()
+plt.savefig(os.path.join(v_directory, 'seq_confusion_matrix.png'), dpi=300)
+plt.close()
+print("     [>] 'seq_confusion_matrix.png saved.")
+
 ### Results for reference:
 """ ==================================================
            FINAL BENCHMARKING RESULTS             
